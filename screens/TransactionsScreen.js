1140692adfb055377bdb326be36ab983434d9471
Nick Carchedi <nick.carchedi@gmail.com>
2019-07-25 16:37:35 -0600

    get example transactions in place

--- screens/TransactionsScreen.js
+++ screens/TransactionsScreen.js
@@ -1,18 +1,52 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, SectionList } from "react-native";
 
 export default function TransactionsScreen() {
+  SectionListItemSeparator = () => {
+    return (
+      <View
+        style={{ height: 0.25, width: "100%", backgroundColor: "#C8C8C8" }}
+      />
+    );
+  };
+
+  TransactionList = () => {
+    return (
+      <SectionList
+        ItemSeparatorComponent={this.SectionListItemSeparator}
+        renderItem={({ item, index }) => (
+          <Text style={styles.transactionListItem} key={index}>
+            {item}
+          </Text>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.transactionDateHeader}>{title}</Text>
+        )}
+        sections={[
+          { title: "Thursday, July 25", data: ["Amazon.com"] },
+          {
+            title: "Wednesday, July 24",
+            data: ["Union Square Cafe", "ACME, Inc.", "The Corner Store"]
+          },
+          { title: "Tuesday, July 23", data: ["Ace Hardware"] }
+        ]}
+        keyExtractor={(item, index) => item + index}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
-        <View style={styles.transactionsContainer}>
-          <Text style={styles.transactionsText}>
-            This will show a list of all transactions.
-          </Text>
-        </View>
+        <Text style={styles.transactionListHeader}>New</Text>
+        <TransactionList />
+        <Text style={[styles.transactionListHeader, { marginTop: 20 }]}>
+          Categorized
+        </Text>
+        <TransactionList />
       </ScrollView>
     </View>
   );
@@ -29,15 +63,20 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 30,
-    paddingHorizontal: 10
+    paddingHorizontal: 20
   },
-  transactionsContainer: {
-    alignItems: "center"
+  transactionListHeader: {
+    fontSize: 34,
+    marginBottom: 10,
+    backgroundColor: "lightgrey"
   },
-  transactionsText: {
+  transactionDateHeader: {
+    fontSize: 22,
+    marginTop: 10
+  },
+  transactionListItem: {
     fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
+    color: "rgba(96, 100, 109, 1)",
+    marginVertical: 15
   }
 });
