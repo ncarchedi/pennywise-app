a568004a13c26a0e4b30fe7397fa2c520cb13574
Nick Carchedi <nick.carchedi@gmail.com>
2019-07-26 16:08:30 -0600

    clean up transactions screen

--- components/TransactionsList.js
+++ components/TransactionsList.js
@@ -1,31 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, SectionList } from "react-native";
+import { StyleSheet, Text, SectionList, View } from "react-native";
 
-export default function TransactionsList({ transactions }) {
+export default function TransactionsList({ header, transactions }) {
+  ListItemSeparator = () => {
+    return (
+      <View style={{ height: 1, width: "100%", backgroundColor: "#f1f1f1" }} />
+    );
+  };
+
   return (
-    <SectionList
-      renderItem={({ item, index }) => (
-        <Text style={styles.transactionsListItem} key={index}>
-          {item}
-        </Text>
-      )}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.transactionsDateHeader}>{title}</Text>
-      )}
-      sections={transactions}
-      keyExtractor={(item, index) => item + index}
-    />
+    <View style={styles.container}>
+      <Text style={styles.transactionsListHeader}>{header}</Text>
+      <SectionList
+        ItemSeparatorComponent={this.ListItemSeparator}
+        renderItem={({ item, index }) => (
+          <View style={styles.transactionsListItem} key={index}>
+            <Text style={styles.transactionsListItemText}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section: { date } }) => (
+          <Text style={styles.transactionsDateHeader}>{date}</Text>
+        )}
+        sections={transactions}
+        keyExtractor={(item, index) => item + index}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  transactionsListHeader: {
+    color: "#fff",
+    fontSize: 34,
+    marginBottom: 10,
+    backgroundColor: "#92BFB1",
+    paddingVertical: 5,
+    paddingHorizontal: 10
+  },
   transactionsDateHeader: {
     fontSize: 22,
-    marginTop: 10
+    marginTop: 10,
+    marginHorizontal: 10
   },
   transactionsListItem: {
+    paddingVertical: 15,
+    marginHorizontal: 10
+  },
+  transactionsListItemText: {
     fontSize: 17,
-    color: "rgba(96, 100, 109, 1)",
-    marginVertical: 15
+    color: "rgba(96, 100, 109, 1)"
   }
 });
--- screens/TransactionsScreen.js
+++ screens/TransactionsScreen.js
@@ -7,17 +7,17 @@ export default class TransactionsScreen extends React.Component {
   state = {
     transactions: {
       new: [
-        { title: "Thursday, July 25", data: ["Amazon.com"] },
+        { date: "Thursday, July 25", data: ["Amazon.com"] },
         {
-          title: "Wednesday, July 24",
+          date: "Wednesday, July 24",
           data: ["Union Square Cafe", "ACME, Inc.", "The Corner Store"]
         },
-        { title: "Tuesday, July 23", data: ["Ace Hardware"] }
+        { date: "Tuesday, July 23", data: ["Ace Hardware"] }
       ],
       categorized: [
-        { title: "Monday, July 22", data: ["Whole Foods"] },
+        { date: "Monday, July 22", data: ["Whole Foods"] },
         {
-          title: "Saturday, July 20",
+          date: "Saturday, July 20",
           data: ["Netflix", "Tequila Sunrise"]
         }
       ]
@@ -33,12 +33,11 @@ export default class TransactionsScreen extends React.Component {
           style={styles.container}
           contentContainerStyle={styles.contentContainer}
         >
-          <Text style={styles.transactionsListHeader}>New</Text>
-          <TransactionsList transactions={transactions.new} />
-          <Text style={[styles.transactionsListHeader, { marginTop: 20 }]}>
-            Categorized
-          </Text>
-          <TransactionsList transactions={transactions.categorized} />
+          <TransactionsList header="New" transactions={transactions.new} />
+          <TransactionsList
+            header="Categorized"
+            transactions={transactions.categorized}
+          />
         </ScrollView>
       </View>
     );
@@ -55,21 +54,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   contentContainer: {
-    paddingTop: 30,
-    paddingHorizontal: 20
-  },
-  transactionsListHeader: {
-    fontSize: 34,
-    marginBottom: 10,
-    backgroundColor: "lightgrey"
-  },
-  transactionsDateHeader: {
-    fontSize: 22,
-    marginTop: 10
-  },
-  transactionsListItem: {
-    fontSize: 17,
-    color: "rgba(96, 100, 109, 1)",
-    marginVertical: 15
+    paddingBottom: 30
   }
 });
