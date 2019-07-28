cd3fd72550b541a08ea525c48efe5157ce419a47
Nick Carchedi <nick.carchedi@gmail.com>
2019-07-28 14:18:09 -0600

    add dummy inputs

--- components/EditTransactionModal.js
+++ components/EditTransactionModal.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, Text } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  DatePickerIOS,
+  TextInput
+} from "react-native";
 import Modal from "react-native-modal";
 
 export default function EditTransactionModal({
@@ -10,17 +17,83 @@ export default function EditTransactionModal({
   return (
     <Modal
       isVisible={isVisible}
-      backdropOpacity={0.95}
+      backdropOpacity={1}
       backdropColor="#fff"
       animationInTiming={50}
       backdropTransitionInTiming={50}
-      style={{ alignItems: "center" }}
     >
-      <Text>{transaction.name}</Text>
-      <Text>{transaction.amount}</Text>
-      <Text>{transaction.category}</Text>
-      <Text>{transaction.date}</Text>
-      <Button title="Go Back" onPress={onExitModal} />
+      <View style={styles.modalContent}>
+        <View style={styles.modalHeader}>
+          <Text style={styles.modalTitle}>Edit Transaction</Text>
+        </View>
+        <View style={styles.modalBody}>
+          <Text style={styles.inputLabel}>Name</Text>
+          <TextInput
+            style={styles.textInput}
+            underlineColorAndroid="transparent"
+            value={transaction.name}
+            onChangeText={name => console.log(name)}
+          />
+          <Text style={styles.inputLabel}>Amount</Text>
+          <TextInput
+            style={styles.textInput}
+            underlineColorAndroid="transparent"
+            value={transaction.amount}
+            onChangeText={amount => console.log(amount)}
+          />
+          <Text style={styles.inputLabel}>Category</Text>
+          <TextInput
+            style={styles.textInput}
+            underlineColorAndroid="transparent"
+            value={transaction.category}
+            onChangeText={category => console.log(category)}
+          />
+          <Text style={styles.inputLabel}>Date</Text>
+          <DatePickerIOS
+            date={new Date(transaction.date)}
+            onDateChange={date => console.log(date)}
+          />
+          <Button
+            style={{ marginBottom: 0 }}
+            title="Done"
+            onPress={onExitModal}
+          />
+        </View>
+      </View>
     </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContent: {
+    paddingTop: 40,
+    flex: 1
+  },
+  modalHeader: {
+    paddingBottom: 10,
+    borderBottomColor: "#f1f1f1",
+    borderBottomWidth: 0.5,
+    width: "100%",
+    alignItems: "center"
+  },
+  modalTitle: {
+    fontWeight: "600",
+    fontSize: 17,
+    color: "rgba(0, 0, 0, 0.9)"
+  },
+  modalBody: {
+    marginTop: 15
+  },
+  textInput: {
+    padding: 10,
+    marginTop: 10,
+    height: 40,
+    borderColor: "#f1f1f1",
+    borderRadius: 5,
+    borderWidth: 1
+  },
+  inputLabel: {
+    fontWeight: "bold",
+    marginTop: 15
+  }
+});
