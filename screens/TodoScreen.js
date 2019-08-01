95db40931d0efce94eccd2948d209c0f7d1f2d11
Nick Carchedi <nick.carchedi@gmail.com>
2019-08-01 06:13:36 -0600

    add "new transaction" button to todo screen

--- screens/TodoScreen.js
+++ screens/TodoScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View, TouchableOpacity } from "react-native";
 
 import TransactionsList from "../components/TransactionsList";
 import EditTransactionModal from "../components/EditTransactionModal";
+import { Ionicons } from "@expo/vector-icons";
+import _ from "lodash";
 
 import { withGlobalContext } from "../GlobalContext";
 
@@ -27,6 +29,16 @@ class TodoScreen extends React.Component {
     this.toggleModal();
   };
 
+  handleAddNewTransaction = () => {
+    const newTransaction = {
+      name: "",
+      amount: null,
+      category: "No Category",
+      date: new Date(_.now())
+    };
+    this.handleTransactionPress(newTransaction);
+  };
+
   render() {
     transactions = this.props.global.transactions;
     const { selectedTransaction, isModalVisible } = this.state;
@@ -48,6 +60,14 @@ class TodoScreen extends React.Component {
             onExitModal={this.toggleModal}
           />
         </ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <TouchableOpacity
+            onPress={this.handleAddNewTransaction}
+            style={styles.newTransactionButton}
+          >
+            <Ionicons name="ios-add" size={40} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -62,5 +82,15 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingBottom: 30
+  },
+  newTransactionButton: {
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    height: 60,
+    borderRadius: 30
   }
 });
