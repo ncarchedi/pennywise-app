b1a7edda6f60702f86be07999b0da5f49b4c78e2
Boris Gordts <borisgordts@hotmail.com>
2019-08-05 23:32:21 -0400

    Getting fake transactions works

--- GlobalContext.js
+++ GlobalContext.js
@@ -4,16 +4,45 @@ import categoriesData from "./categories.json";
 
 const GlobalContext = React.createContext({});
 
+const BACKEND_DEV = 'http://localhost:5001/conscious-spending-backend/us-central1/getPlaidTransactions'
+const BACKEND_PROD = 'https://us-central1-conscious-spending-backend.cloudfunctions.net/getPlaidTransactions'
+
+const BACKEND_URL = BACKEND_PROD
+
 export class GlobalContextProvider extends React.Component {
   state = {
     transactions: [],
-    categories: []
+    categories: [],
+    access_token: ''
   };
 
   componentDidMount() {
     this.setState({
       transactions: transactionsData,
       categories: categoriesData
+    });
+  }
+
+  getAccessTokenFromPublicToken = (publicToken) => {
+    console.log('public token');
+    console.log(publicToken);
+    
+    return fetch(BACKEND_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        public_token: publicToken,
+      }),
+    }).then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
     });
   }
 
@@ -30,10 +59,11 @@ export class GlobalContextProvider extends React.Component {
     return (
       <GlobalContext.Provider
         value={{
-          ...this.state
+          ...this.state,
           // Every function to update the state should be listed here too:
           // switchToOnline: this.switchToOnline,
           // switchToOffline: this.switchToOffline
+          getAccessTokenFromPublicToken: this.getAccessTokenFromPublicToken
         }}
       >
         {this.props.children}
--- screens/AnalyticsScreen.js
+++ screens/AnalyticsScreen.js
@@ -1,28 +1,72 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
 
 import { withGlobalContext } from "../GlobalContext";
+
+import PlaidAuthenticator from 'react-native-plaid-link';
 
 class AnalyticsScreen extends React.Component {
   static navigationOptions = {
     title: "Analytics"
   };
 
+  state = {
+    isReady: false
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-        >
-          <View style={styles.analyticsContainer}>
-            <Text style={styles.analyticsText}>
-              This will show some spending analytics.
-            </Text>
-          </View>
-        </ScrollView>
-      </View>
-    );
+    // return (
+    //   <View style={styles.container}>
+    //     <ScrollView
+    //       style={styles.container}
+    //       contentContainerStyle={styles.contentContainer}
+    //     >
+    //       <View style={styles.analyticsContainer}>
+    //         <Text style={styles.analyticsText}>
+    //           This will show some spending analytics.
+    //         </Text>
+    //       </View>
+    //     </ScrollView>
+    //   </View>
+    // );
+
+    if(!this.state.isReady) {
+      return <PlaidAuthenticator
+        onMessage={this.onMessage}
+        publicKey="aef79d0fac8493ad10a8760b0c01a6"
+        env="sandbox"
+        product="transactions"
+        clientName="Conscious Spending Client"
+        selectAccount={false}
+      />
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>
+            Ready! Public token: {this.state.metadata.public_token}
+          </Text>
+          <Button
+            onPress={this.cleanState}
+            title="Clean State"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+        </View>
+      )
+    }
+  }
+
+  onMessage = (data) => {
+    if(data.action.includes('connected')) {
+      this.props.global.getAccessTokenFromPublicToken(data.metadata.public_token)
+      this.setState({...data, isReady: true})
+    } else (
+      this.setState({data})
+    )
+  }
+
+  cleanState = () => {
+    this.setState({isReady: false})
   }
 }
 
