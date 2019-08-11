027e04b4961eb47c7c3f85da12bd43f326bc018f
Nick Carchedi <nick.carchedi@gmail.com>
2019-08-11 17:03:09 -0600

    get dummy toggle working

--- components/EditTransactionModal.js
+++ components/EditTransactionModal.js
@@ -7,98 +7,128 @@ import {
   View,
   DatePickerIOS,
   TextInput,
-  PickerIOS
+  PickerIOS,
+  Switch
 } from "react-native";
 import Modal from "react-native-modal";
 
 import { withGlobalContext } from "../GlobalContext";
 
-function EditTransactionModal({
-  transaction,
-  isVisible,
-  onExitModal,
-  onChangeTransaction,
-  ...props
-}) {
-  // TODO: https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
-  const { name, amount, category, date } = transaction;
+class EditTransactionModal extends React.Component {
+  state = {
+    isIncome: false
+  };
 
-  return (
-    <Modal
-      isVisible={isVisible}
-      backdropOpacity={1}
-      backdropColor="#fff"
-      animationInTiming={400}
-      animationOutTiming={400}
-      backdropTransitionInTiming={0}
-      backdropTransitionOutTiming={0}
-    >
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.modalContent}>
-          <View style={styles.modalHeader}>
-            <Text style={styles.modalTitle}>Edit Transaction</Text>
-          </View>
-          <View style={styles.modalBody}>
-            <View style={{ flexDirection: "row" }}>
-              <Text style={styles.dollarSign}>$</Text>
+  toggleSwitch = () => {
+    const { isIncome } = this.state;
+
+    this.setState({ isIncome: !isIncome });
+  };
+
+  render() {
+    const { isIncome } = this.state;
+
+    const {
+      transaction,
+      isVisible,
+      onExitModal,
+      onChangeTransaction,
+      ...props
+    } = this.props;
+
+    // TODO: https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+    const { name, amount, category, date } = transaction;
+
+    return (
+      <Modal
+        isVisible={isVisible}
+        backdropOpacity={1}
+        backdropColor="#fff"
+        animationInTiming={400}
+        animationOutTiming={400}
+        backdropTransitionInTiming={0}
+        backdropTransitionOutTiming={0}
+      >
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalTitle}>Edit Transaction</Text>
+            </View>
+            <View style={styles.modalBody}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ alignSelf: "center" }}>
+                  <Switch
+                    value={isIncome}
+                    onValueChange={() => this.toggleSwitch()}
+                  />
+                </View>
+                <Text
+                  style={[
+                    styles.dollarSign,
+                    isIncome ? styles.incomePrefix : styles.expensePrefix
+                  ]}
+                >
+                  {isIncome ? "+$" : "-$"}
+                </Text>
+                <TextInput
+                  style={styles.amountInput}
+                  value={String(amount)}
+                  placeholder="24.99"
+                  onChangeText={amount => onChangeTransaction("amount", amount)}
+                  keyboardType={"numbers-and-punctuation"}
+                  autoCorrect={false}
+                  onSubmitEditing={() => {
+                    this.nameInput.focus();
+                  }}
+                  blurOnSubmit={false}
+                  autoFocus
+                />
+              </View>
+              <Text style={styles.inputLabel}>Name</Text>
               <TextInput
-                style={styles.amountInput}
-                value={String(amount)}
-                placeholder="24.99"
-                onChangeText={amount => onChangeTransaction("amount", amount)}
-                keyboardType={"numbers-and-punctuation"}
-                autoCorrect={false}
-                onSubmitEditing={() => {
-                  this.nameInput.focus();
+                style={styles.textInput}
+                value={name}
+                placeholder="New Transaction"
+                onChangeText={name => onChangeTransaction("name", name)}
+                ref={input => {
+                  this.nameInput = input;
                 }}
-                blurOnSubmit={false}
-                autoFocus
+                autoCorrect={false}
+              />
+              <Text style={styles.inputLabel}>Category</Text>
+              <PickerIOS
+                selectedValue={category}
+                onValueChange={category =>
+                  onChangeTransaction("category", category)
+                }
+              >
+                {props.global.categories.map(category => {
+                  return (
+                    <PickerIOS.Item
+                      key={category.id}
+                      value={category.label}
+                      label={category.label}
+                    />
+                  );
+                })}
+              </PickerIOS>
+              <Text style={styles.inputLabel}>Date</Text>
+              <DatePickerIOS
+                date={date}
+                onDateChange={date => onChangeTransaction("date", date)}
+                mode="date"
+              />
+              <Button
+                style={{ marginBottom: 0 }}
+                title="Save Changes"
+                onPress={onExitModal}
               />
             </View>
-            <Text style={styles.inputLabel}>Name</Text>
-            <TextInput
-              style={styles.textInput}
-              value={name}
-              placeholder="New Transaction"
-              onChangeText={name => onChangeTransaction("name", name)}
-              ref={input => {
-                this.nameInput = input;
-              }}
-              autoCorrect={false}
-            />
-            <Text style={styles.inputLabel}>Category</Text>
-            <PickerIOS
-              selectedValue={category}
-              onValueChange={category =>
-                onChangeTransaction("category", category)
-              }
-            >
-              {props.global.categories.map(category => {
-                return (
-                  <PickerIOS.Item
-                    key={category.id}
-                    value={category.label}
-                    label={category.label}
-                  />
-                );
-              })}
-            </PickerIOS>
-            <Text style={styles.inputLabel}>Date</Text>
-            <DatePickerIOS
-              date={date}
-              onDateChange={date => onChangeTransaction("date", date)}
-              mode="date"
-            />
-            <Button
-              style={{ marginBottom: 0 }}
-              title="Save Changes"
-              onPress={onExitModal}
-            />
           </View>
-        </View>
-      </ScrollView>
-    </Modal>
-  );
+        </ScrollView>
+      </Modal>
+    );
+  }
 }
 
 export default withGlobalContext(EditTransactionModal);
@@ -132,8 +162,15 @@ const styles = StyleSheet.create({
     borderWidth: 1
   },
   dollarSign: {
+    marginLeft: "auto",
     fontSize: 50,
     marginRight: 5
+  },
+  incomePrefix: {
+    color: "green"
+  },
+  expensePrefix: {
+    color: "red"
   },
   amountInput: {
     fontSize: 50
